6f6674e0aead4656f9d3c82405568baa40cb90cf
stevezhuravel <33463234+stevezhuravel@users.noreply.github.com>
2019-12-26 23:32:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,39 @@ const ticTacToeGame = new TicTacToeGame();
 ticTacToeGame.start();
 
 function TicTacToeGame(){
-const games = new Games();
-const personPlayer = new personPlayer(games);
-const machinePlayer = new MachinePlayer(games);
+const game = new Game();
+const humanPlayer = new HumanPlayer(game);
+const computerPlayer = new ComputerPlayer(game);
 let turn =0;
 
 this.start = function(){
     const config = { childList: true};
     const observer = new MutationObserver(() => takeTurn())
-    games.positions.forEach((el) => observer.observe(el, config));
+    game.positions.forEach((el) => observer.observe(el, config));
     takeTurn();
 
     }
     function takeTurn(){
-        if(games.winner()){
+        if(game.chechForWinner()){
             return;
         }
         if(turn % 2 === 0 ){
-            personPlayer.takeTurn();
+            humanPlayer.takeTurn();
 
         } else {
-            machinePlayer.takeTurn();
+            computerPlayer.takeTurn();
         }
 
         turn++;
     }
 }
-function Board(){
+function Game(){
 this.positions = Array.from(document.querySelectorAll('.col'));
 
 
-this.winner = function(){
+this.chechForWinner = function(){
     let winner = false;
-    const checkingCombination = [
+    const winnerCombination = [
         [0,1,2],
         [3,4,5],
         [6,7,8],
@@ -44,19 +44,19 @@ this.winner = function(){
         [1,4,7],
         [2,5,8]
     ];
-    const leftPositions = this.leftPositions;
+    const positions = this.positions;
 
-   checkingCombination.forEach((winnerCombo) => {
-        const pos0InnerText = leftPositions[winnerCombo[0]].innerText;
-        const pos1InnerText = leftPositions[winnerCombo[1]].innerText;
-        const pos2InnerText = leftPositions[winnerCombo[2]].innerText;
+   winnerCombination.forEach((winnerCombo) => {
+        const pos0InnerText = positions[winnerCombo[0]].innerText;
+        const pos1InnerText = positions[winnerCombo[1]].innerText;
+        const pos2InnerText = positions[winnerCombo[2]].innerText;
         const isWinningCombo = pos0InnerText !== '' & 
         pos0InnerText === pos1InnerText & pos1InnerText === pos2InnerText;
 
         if(isWinningCombo){
             winner = true;
             winnerCombo.forEach((index) =>{
-                leftPositions[index].className += 'winner';
+            positions[index].className += 'winner';
             })
         }
 
@@ -66,26 +66,26 @@ this.winner = function(){
 
 }
 
-function HumanPlayer(games){
+function HumanPlayer(game){
 
     this.takeTurn = function(){
     
-        games.leftPositions
+        game.positions
         .forEach(el => el.addEventListener('click', handleTurnTaken));
     }
     
     function handleTurnTaken(event){
        event.target.innerText = 'X';
-       games.leftPositions
+       game.positions
        .forEach(el => el.removeEventListener('click', handleTurnTaken));
     }
 }
 
-function ComputerPlayer(games){
+function ComputerPlayer(game){
 
     this.takeTurn = function(){
         const availablePosition =
-        games.leftPositions.filter((p) => p.innerText === '');
+        game.positions.filter((p) => p.innerText === '');
         const move = Math.floor(Math.random() * availablePosition.length); 
         availablePosition[move].innerText = 'O';
         
